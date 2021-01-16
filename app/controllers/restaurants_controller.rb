5db9b6ca34893c1f16a32dd067b43ab215f4d07b
Kseniia Novinskaia <kseniia.novinskaia@gmail.com>
2021-01-16 11:13:43 +0100

    All restaurants with Review model

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,26 @@
-class PagesController < ApplicationController
+class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurants.all
+    @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 
+  def new
+    @restaurant = Restaurant.new
+  end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-get 'restaurants', to: 'restaurants#index'
-get 'restaurants/:id', to: 'restaurants#show', as: restaurant
-get 'restaurants/new', to: 'restaurants#new'
-post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # post 'restaurants', to: 'restaurants#create'
+
+  resources :restaurants, only: [ :index, :new, :show, :create ] do
+    resources :reviews, only: [ :new, :create ]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# resources :restaurants do
+#     resources :reviews, only: [ :new, :create ]
+#   end
